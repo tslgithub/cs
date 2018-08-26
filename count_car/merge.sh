ad00f2dbe2ba0ed315388d105281e1a5909d9489
tsl <mymailwith163@163.com>
2018-08-26 22:03:53 +0800

    update merge.sh

--- count_car/merge.sh
+++ count_car/merge.sh
@@ -1,10 +1,13 @@
+echo "input video name"
+read video
+
 ffmpeg \
-  -i /home/tsl/keras-yolo3/data/result/44.mp4 \
-  -i /home/tsl/keras-yolo3/data/result/44_1.mp4 \
+  -i /home/tsl/keras-yolo3/data/result/$video'_1.mp4' \
+  -i /home/tsl/keras-yolo3/data/result/$video'_2.mp4' \
   -filter_complex '[0:v]pad=iw*2:ih[int];[int][1:v]overlay=W/2:0[vid]' \
   -map [vid] \
   -c:v libx264 \
   -crf 23 \
   -preset veryfast \
-  output.mp4
+  /home/tsl/keras-yolo3/data/result/$video'_merge.mp4'
 
